3c31dbb7e5cf9318ec4b62cfc02a77ea5a5e79ba
ChathuraSamarakoon <chathurasamarakoon849495@gmail.com>
2026-05-17 21:24:22 +0530

    move oders page and tracking page to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Route, Routes } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage' 
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
+
+
 
 function App() {
  
@@ -9,6 +13,9 @@ function App() {
     <Routes>
       <Route index element={<HomePage/>}/>
       <Route path='Checkout' element={<CheckoutPage/>}/>
+      <Route path='orders' element={<OrdersPage/>}/>
+      <Route path='tracking' element={<TrackingPage/>}/>
+
       
     </Routes>
     
